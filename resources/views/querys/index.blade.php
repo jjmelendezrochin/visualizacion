915666cd9b5b755add59df52b373749eaeae0d55
Juan Jose Melendez Rochin <jmelendez@jjcorp.com.mx>
2024-02-21 10:20:07 -0600

    Se colocan los elementos gráficos para la pantalla de consultas incluyendo caja, botón y estilos

--- resources/views/querys/index.blade.php
+++ resources/views/querys/index.blade.php
@@ -5,25 +5,32 @@
         </h2>
     </x-slot>
 
+
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="POST" action="#">
-                        @csrf
-                        <textarea name="mensaje"
-                            class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
-                            placeholder="{{ __('What\'s on your mind?') }}">{{ old('mensaje') }}</textarea>
-                        <x-input-error :messages="$errors->get('mensaje')" />
-                        @error('mensaje')
-                            {{ $message }}
-                        @enderror
-                        <br>
-                        <x-primary-button class="mt-4 center">
-                            Consulta
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+
+            <div
+                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <div
+                    class="flex flex-row block p-4 ps-10 text-sm text-white-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-white-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <div>
+                        <input type="text" id="simple-search" size="30px"
+                            class="flex flex-row block p-4 ps-10 text-sm text-white-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-white-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="{{ __('What are you looking for?') }}" required />
+                    </div>
+                    <div style="padding-left: .10in; padding-top: .10in">
+
+                        <x-primary-button>
+                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
                         </x-primary-button>
-                    </form>
+
+                    </div>
                 </div>
             </div>
+
         </div>
+    </div>
+
 </x-app-layout>
