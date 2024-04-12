99c25394affdf9997b173073d471cbd70c353cc3
Juan Jose Melendez Rochin <jmelendez@jjcorp.com.mx>
2024-04-11 18:15:54 -0600

    Combinación de proyectos 4

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -98,9 +98,26 @@
         <div class="max-w-full mx-auto">
             <div class="bg-inherit dark:bg-gray-800 overflow-hidden  sm:rounded-lg">
 
-                <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
-                    Lista de archivos en carpeta {{ $datos[2] }}
-                </h2>
+                <div class="flex flex-row p-1 text-gray-900 dark:text-gray-100 ">
+                    <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
+                        Lista de archivos en carpeta {{ $datos[2] }}
+                    </h2>
+                        &NonBreakingSpace;
+                        &NonBreakingSpace;
+                        &NonBreakingSpace;
+                        &NonBreakingSpace;
+                        &NonBreakingSpace;
+                    <x-primary-button data-toggle="tooltip"
+                            data-placement="bottom" title="Ver Carpetas"
+                        onclick="window.location='/folders'">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75 14.25 12m0 0 2.25 2.25M14.25 12l2.25-2.25M14.25 12 12 14.25m-2.58 4.92-6.374-6.375a1.125 1.125 0 0 1 0-1.59L9.42 4.83c.21-.211.497-.33.795-.33H19.5a2.25 2.25 0 0 1 2.25 2.25v10.5a2.25 2.25 0 0 1-2.25 2.25h-9.284c-.298 0-.585-.119-.795-.33Z" />
+                          </svg>
+
+                          {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10"> <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" /> </svg> --}}
+
+                    </x-primary-button>
+                </div>
 
                 <form method="GET" action="{{ route('dashboard') }}" name="form1" id="form1">
                     @csrf
